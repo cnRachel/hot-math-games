427b264788519b1ac86a762bcdf1e0580a024105
Rachel Chen <rachelchen110@gmail.com>
2023-01-28 11:57:45 -0800

    html, css, js

---
+++ js/script.js
@@ -0,0 +1,56 @@
+let menuBtn = document.querySelector('#menu-btn');
+let searchBtn = document.querySelector('#search-btn');
+let themeBtn = document.querySelector('#theme-btn');
+
+let navbar = document.querySelector('.navbar');
+let searchForm = document.querySelector('.search-form');
+
+menuBtn.onclick = () =>{
+  navbar.classList.toggle('active');
+  searchForm.classList.remove('active');
+}
+
+searchBtn.onclick = () =>{
+  searchForm.classList.toggle('active');
+  navbar.classList.remove('active');
+}
+
+window.onscroll = () =>{
+  navbar.classList.remove('active');
+  searchForm.classList.remove('active');
+}
+
+themeBtn.onclick = () => {
+  themeBtn.classList.toggle('fa-sun');
+  if(themeBtn.classList.contains('fa-sun')){
+    document.body.classList.add('active');
+  }else{
+    document.body.classList.remove('active');
+  }
+}
+
+/////////////////////////////////////////////////////////////////////
+
+let previewContainer = document.querySelector('.products-preview');
+let previewBox = previewContainer.querySelectorAll('.preview');
+
+document.querySelectorAll('.box-container .box').forEach(box =>{
+  box.onclick = () =>{
+    previewContainer.style.display = 'flex';
+    let name = box.getAttribute('data-name');
+    previewBox.forEach(preview =>{
+      let target = preview.getAttribute('data-target');
+      if(name == target){
+        preview.classList.add('active');
+      }
+    });
+  };
+});
+
+previewBox.forEach(close =>{
+  close.querySelector('.fa-times').onclick = () =>{
+    close.classList.remove('active');
+    previewContainer.style.display = 'none';
+    location.reload();
+  };
+});
